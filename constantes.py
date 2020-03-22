2cb1e235ef3bca3758aefbec69c713e0530137db
Simon Libotte <libotte.simon@gmail.com>
2020-03-22 12:06:41 +0100

    fichier constantes.py

---
+++ constantes.py
@@ -0,0 +1,19 @@
+#window setting
+
+nb_sprite = 15
+sprite_size = 30
+side_window = nb_sprite * sprite_size
+
+#window customization
+
+title_window = "macgyver"
+image_icone_perso = "ressource/images/macgyver.png"
+
+#list of game images
+#a définir
+
+image_homepage = "ressource/images/menu.png"
+image_background = "a définir"
+image_wall = "../ressource/images/wall.png"
+image_start = "a définir"
+image_finish = "a définir"
